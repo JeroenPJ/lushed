3ffdf13ae13b7bc01c7796f86e2981f6db05cb11
Jeroen Jagt <jeroenpjagt@gmail.com>
2018-06-06 20:25:31 +0200

    added home, /navbar, styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def navbar
+    html = render_to_string template: 'pages/_navbar', layout: false
+    render json: {
+      html: html
+    }
+  end
 end
