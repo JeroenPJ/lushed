7f69dda820a90abea24a54cbb485359e58d58ae6
Jeroen Jagt <jeroenpjagt@gmail.com>
2018-06-16 13:31:56 +0200

    Added /words

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,4 +8,11 @@ class PagesController < ApplicationController
       html: html
     }
   end
+
+
+  def words
+    @body_classes = ["black", "full-height"]
+    @words = ['flower', 'tree', 'lush', 'nature', 'outside']
+    # @words = "http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&minLength=5&maxLength=15&limit=5&api_key=#{ENV['WORDNIK_API_KEY']"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'navbar', to: 'pages#navbar', as: 'navbar'
+  get 'words', to: 'pages#words', as: 'words'
   root to: 'pages#home'
 end
