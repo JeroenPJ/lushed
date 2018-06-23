0c3c66fe745bd2e78e79aebaa011eaa33a395694
Jeroen Jagt <jeroenpjagt@gmail.com>
2018-06-23 18:36:44 +0200

    Hooked up /words to Wordnik API

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class PagesController < ApplicationController
   def home
   end
@@ -11,8 +13,13 @@ class PagesController < ApplicationController
 
   def words
     @body_classes = ["black", "full-height"]
-    @words = ['flower', 'tree', 'lush', 'nature', 'outside']
-    # @words = "http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&minLength=5&maxLength=15&limit=5&api_key=#{ENV['WORDNIK_API_KEY']"
+
+    # @words = ['flower', 'tree', 'lush', 'nature', 'outside']
+
+    included_pos = "noun,adjective,verb,adverb,interjection,preposition,idiom,imperative,noun-plural,verb-intransitive,verb-transitive"
+    # all possible POS: noun, adjective, verb, adverb, interjection, pronoun, preposition, abbreviation, affix, article, auxiliary-verb, conjunction, definite-article, family-name, given-name, idiom, imperative, noun-plural, noun-posessive, past-participle, phrasal-prefix, proper-noun, proper-noun-plural, proper-noun-posessive, suffix, verb-intransitive, verb-transitive
+    url = "http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&minLength=5&maxLength=15&limit=5&includePartOfSpeech=#{included_pos}&api_key=#{ENV['WORDNIK_API_KEY']}"
+    @words = JSON.load(open(url))
   end
 
   def quotes
